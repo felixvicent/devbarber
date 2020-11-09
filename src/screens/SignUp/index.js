abfc837a1939c27f45729696eaca3f6f560dc84e
Felix Vicente <felixvicent1306@gmail.com>
2020-11-09 15:06:34 -0300

    Config navigation stack

---
+++ src/screens/SignUp/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from "react-native";
+
+import { Container } from './styles';
+
+export default () => (
+  <Container>
+    <Text>SignUp</Text>
+  </Container>
+)
