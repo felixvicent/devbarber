376b82cf2ac40a09aa28a666c8a1f1138d26cd54
Felix Vicente <felixvicent1306@gmail.com>
2020-11-09 16:44:39 -0300

    Making SignIn screen

--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -1,10 +1,55 @@
-import React from 'react';
-import { Text } from "react-native";
+import React, { useState } from 'react';
+import { useNavigation } from '@react-navigation/native'
 
-import { Container } from './styles';
+import BarberLogo from '../../assets/barber.svg'
+import EmailIcon from '../../assets/email.svg'
+import LockIcon from '../../assets/lock.svg'
 
-export default () => (
-  <Container>
-    <Text>SignIn</Text>
-  </Container>
-)
+import { Container, InputArea, CustomButton, CustomButtonText, SignMessageButton, SignMessageButtonText, SignMessageButtonTextBold } from './styles';
+import SignInput from '../../components/SignInput';
+
+export default () => {
+  const navigation = useNavigation();
+
+  const [emailField, setEmailField] = useState('');
+  const [passwordField, setPasswordField] = useState('');
+
+  const handleMessageButtonClick = () => {
+    navigation.reset({ routes: [{ name: 'SignUp' }] })
+  }
+
+  const handleSignClick = () => {
+
+  }
+
+  return (
+    <Container>
+      <BarberLogo width='100%' height='160' />
+
+      <InputArea>
+        <SignInput
+          IconSvg={EmailIcon}
+          placeholder='Digite seu email'
+          value={ emailField }
+          onChangeText={ text => setEmailField(text) }
+        />
+        <SignInput
+          IconSvg={LockIcon}
+          placeholder='Digite sua senha'
+          value={ passwordField }
+          password
+          onChangeText={ text => setPasswordField(text) }
+        />
+
+        <CustomButton onPress={ handleSignClick } >
+          <CustomButtonText>LOGIN</CustomButtonText>
+        </CustomButton>
+      </InputArea>
+
+      <SignMessageButton onPress={ handleMessageButtonClick } >
+        <SignMessageButtonText>Ainda não possui uma conta?</SignMessageButtonText>
+        <SignMessageButtonTextBold>Cadastre-se</SignMessageButtonTextBold>
+      </SignMessageButton>
+    </Container>
+  )
+}
