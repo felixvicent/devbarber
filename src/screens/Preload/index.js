da281ef940077d5481290efc24b6ce123f92dded
Felix Vicente <felixvicent1306@gmail.com>
2020-11-10 13:51:17 -0300

    Config tab navigaor

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -1,12 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext } from 'react';
 import { useNavigation } from '@react-navigation/native'
 import AsyncStorage from '@react-native-community/async-storage';
+import { UserContext } from '../../contexts/UserContext';
+
 
 import { Container, LoadingIcon } from './styles';
 
 import BarberLogo from '../../assets/barber.svg';
 
+import Api from '../../Api';
+
 export default () => {
+  const { dispatch: userDispatch } = useContext(UserContext);
   const navigation = useNavigation();
 
   useEffect(() => {
@@ -14,7 +19,21 @@ export default () => {
       const token = await AsyncStorage.getItem('token');
 
       if(token){
-        // validar o token
+        let res = await Api.checkToken(token);
+
+        if(res.token) {
+          await AsyncStorage.setItem('token', res.token);
+
+          userDispatch({
+            type: 'setAvatar',
+            payload: { avatar: res.data.avatar }
+          });
+
+          navigation.reset({ routes: [{ name: 'MainTab' }] })
+        }
+        else {
+          navigation.navigate('SignIn');
+        }
       }
       else {
         navigation.navigate('SignIn');
